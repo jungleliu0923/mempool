5d23d72c850716b5ef5d4da2d14fa88665afaafb
liujun05 <liujun0923@zju.edu.cn>
2014-02-16 16:04:12 +0800

    add Makefile

---
+++ Makefile
@@ -0,0 +1,2 @@
+target:MemPool.h MemPool.cpp
+	g++ -c MemPool.h MemPool.cpp -O2
--- MemPool.h
+++ MemPool.h
@@ -20,6 +20,7 @@
 #ifndef  __MEMPOOL_H_
 #define  __MEMPOOL_H_
 
+#include <cstdio>
 #include <vector>
 #include <string>
 using namespace std;
